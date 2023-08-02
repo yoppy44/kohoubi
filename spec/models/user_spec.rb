d19dccfe0decece28f7bb1a19ae1391ef418c5b8
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-08-02 17:35:30 +0900

    テストコード実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'なまえ、メールアドレス、ヒミツのカギ2個が存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+  end
+  context 'ユーザー登録ができない時' do
+    it 'なまえが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+    end
+    it 'メールアドレスが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールを入力してください")
+    end
+    it '重複したメールアドレスが存在する場合は登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
+    end
+    it 'メールアドレスは@を含まないと登録できない' do
+      @user.email = 'testmail'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールは不正な値です")
+    end
+    it 'ヒミツのカギが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードを入力してください")
+    end
+    it 'ヒミツのカギが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+    end
+    it 'ヒミツのカギに半角のアルファベットと数字が入っていなければ登録できない' do
+      @user.password = 'testpass'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは半角英数字で入力してください")
+    end
+    it 'ヒミツのカギに数字のみでは登録できない' do
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは半角英数字で入力してください")
+    end
+    it 'ヒミツのカギに全角文字を含めては登録できない' do
+      @user.password = 'ああああああ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは半角英数字で入力してください")
+    end
+    it 'ヒミツのカギともういっかいが不一致では登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+    end
+  end
 end
