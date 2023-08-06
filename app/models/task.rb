9321c0da588e5884cce404f366971caba25f0ab2
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-08-06 13:14:31 +0900

    やること一覧表示機能の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,7 @@ class Task < ApplicationRecord
   belongs_to :score
 
   validates :content, presence: true
+  validates :content, uniqueness: { scope: :content , message: "はすでにあります" }
   validates :content, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
 
 end
